4123dd6ea1dc8c8e58e9a07bfa98f8e1ff794ce5
Vincent Arnaud <varnaud@student.42.us.org>
2017-04-24 22:38:45 -0700

    fix color display

--- checker.c
+++ checker.c
@@ -6,40 +6,39 @@
 /*   By: varnaud <varnaud@student.42.us.org>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/19 22:04:36 by varnaud           #+#    #+#             */
-/*   Updated: 2017/04/24 15:40:34 by varnaud          ###   ########.fr       */
+/*   Updated: 2017/04/24 22:37:44 by varnaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "checker.h"
 
-static int		print_stack(t_stack *a, t_stack *b, const char *c,
-													const char *op)
+static void		print_stack(t_stack *a, t_stack *b, t_flag *f, const char *op)
 {
 	int		i;
+	char	c[42];
 
-	i = 0;
-	ft_printf("-------------top-------------\n");
-	while (i < a->size && i < b->size)
-	{
-		ft_printf("% 11d  --- %-13d%s%-s%s\n", a->array[a->size - i - 1],
-			b->array[b->size - i - 1], c,
-			(i == GTR(a->size, b->size) / 2) ? op : "", "\e[0m\e[39m");
-		i++;
-	}
+	ft_strcpy(c, f->flag & FLAG_C ? "\e[32m" : "");
+	c[3] = f->flag & FLAG_C && *op == 'E' ? '1' : c[3];
+	ft_strcat(c, op);
+	ft_strcat(c, f->flag & FLAG_C ? "\e[0m\e[39m" : "");
+	i = -1;
+	ft_fprintf(f->fdout, "-------------top-------------\n");
+	while (++i < a->size && i < b->size)
+		ft_fprintf(f->fdout, "% 11d  --- %-13d%-s\n", a->array[a->size - i - 1],
+		b->array[b->size - i - 1], (i == GTR(a->size, b->size) / 2) ? c : "");
 	while (i < a->size)
 	{
-		ft_printf("% 11d  ---%s%14s%-s%s\n", a->array[a->size - i - 1], c, "",
-			(i == GTR(a->size, b->size) / 2) ? op : "", "\e[0m\e[39m");
+		ft_fprintf(f->fdout, "% 11d  ---%14s%-s\n", a->array[a->size - i - 1],
+			"", (i == GTR(a->size, b->size) / 2) ? c : "");
 		i++;
 	}
 	while (i < b->size)
 	{
-		ft_printf("             --- %-13d%s%-s%s\n", b->array[b->size - i - 1],
-			c, (i == GTR(a->size, b->size) / 2) ? op : "", "\e[0m\e[39m");
+		ft_fprintf(f->fdout, "%13s--- %-13d%-s\n", "", b->array[b->size - i - 1]
+			, (i == GTR(a->size, b->size) / 2) ? c : "");
 		i++;
 	}
-	ft_printf("------------bottom-----------\n");
-	return (0);
+	ft_fprintf(f->fdout, "------------bottom-----------\n");
 }
 
 static int		cleanup(t_oplst *lst)
@@ -61,9 +60,11 @@ static int		cleanup(t_oplst *lst)
 static void		check_stack(t_stack *a, t_flag *flag)
 {
 	if (is_sort(a->array, a->size, flag->stack->array, flag->stack->size))
-		ft_printf(flag->flag & FLAG_C ? "\e[1m\e[92mOK\e[0m\e[39m\n" : "OK\n");
+		ft_fprintf(flag->fdout,
+				flag->flag & FLAG_C ? "\e[1m\e[92mOK\e[0m\e[39m\n" : "OK\n");
 	else
-		ft_printf(flag->flag & FLAG_C ? "\e[1m\e[91mKO\e[0m\e[39m\n" : "KO\n");
+		ft_fprintf(flag->fdout,
+				flag->flag & FLAG_C ? "\e[1m\e[91mKO\e[0m\e[39m\n" : "KO\n");
 }
 
 static int		eval_operation(t_oplst *cur, t_stack *a, t_stack *b,
@@ -105,7 +106,7 @@ int				checker(t_stack *a, t_stack *b, int fd, t_flag *flag)
 	t_oplst	**cur;
 
 	if (flag->flag & FLAG_V && !(lst = NULL))
-		print_stack(a, b, flag->flag & FLAG_C ? "\e[1m\e[91m" : "", "");
+		print_stack(a, b, flag, "");
 	lst = NULL;
 	cur = &lst;
 	while ((r = gnl(fd, &line)))
@@ -115,9 +116,9 @@ int				checker(t_stack *a, t_stack *b, int fd, t_flag *flag)
 		if (flag->flag & FLAG_V)
 		{
 			if ((r = execute(line, a, b)))
-				print_stack(a, b, flag->flag & FLAG_C ? "\e[31m" : "", "Error");
+				print_stack(a, b,flag, "Error");
 			else
-				print_stack(a, b, flag->flag & FLAG_C ? "\e[32m" : "", line);
+				print_stack(a, b, flag, line);
 		}
 		else if (add_op(&cur, line))
 			return (!cleanup(lst));
